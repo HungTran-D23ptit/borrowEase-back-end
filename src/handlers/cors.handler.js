1eefc14aeacc4b45d58bb6a66912adab4d3378da
Tran Duy Hung <HungTD.B23CC083@stu.ptit.edu.vn>
2026-04-16 01:17:10 +0700

    up

--- src/handlers/cors.handler.js
+++ src/handlers/cors.handler.js
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import {APP_URL_CLIENT, OTHER_URLS_CLIENT} from '@/configs'
+import { APP_URL_CLIENT, OTHER_URLS_CLIENT } from '@/configs'
 
 const isDev = process.env.NODE_ENV !== 'production'
 
@@ -9,7 +9,7 @@ export const corsOptions = {
         : [
             OTHER_URLS_CLIENT,
             ...APP_URL_CLIENT,
-            'https://borrowease-frontend.netlify.app',
+            'https://borrow-ease-manage.netlify.app',
             'http://10.0.2.2:8080',
         ],
     credentials: true,
