e07df097ba3e613874e87392412e03fad8099329
Tran Duy Hung <HungTD.B23CC083@stu.ptit.edu.vn>
2026-04-24 10:57:27 +0700

    up

--- src/configs/cloudinary.js
+++ src/configs/cloudinary.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('Initializing Cloudinary with Cloud Name:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('API Key present:', !!process.env.CLOUDINARY_API_KEY);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
