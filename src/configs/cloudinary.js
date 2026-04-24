f93289f1815c247ceb537a078476dac2bc502ec9
Tran Duy Hung <HungTD.B23CC083@stu.ptit.edu.vn>
2026-04-24 11:00:45 +0700

    up

--- src/configs/cloudinary.js
+++ src/configs/cloudinary.js
@@ -7,9 +7,9 @@ console.log('Initializing Cloudinary with Cloud Name:', process.env.CLOUDINARY_C
 console.log('API Key present:', !!process.env.CLOUDINARY_API_KEY);
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: String(process.env.CLOUDINARY_CLOUD_NAME),
+  api_key: String(process.env.CLOUDINARY_API_KEY),
+  api_secret: String(process.env.CLOUDINARY_API_SECRET),
 });
 
 export default cloudinary;
